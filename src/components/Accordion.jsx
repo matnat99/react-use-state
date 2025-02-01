e3ec4882707abb8adc8d597899ec789c086888a9
Mattia Natale <mattianatale07@gmail.com>
2025-02-01 18:57:05 +0100

    feat: add toggleAccordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 export default function Accordion({ title, description }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen((current) => !current);
+  };
+
   return (
     <div className="accordion">
-      <div className="accordion-title">
+      <div className={`accordion-title ${isOpen ? "open" : ""}`}>
         <h3>{title}</h3>
-        <button className="accordion-btn">+</button>
+        <button className="accordion-btn" onClick={toggleAccordion}>
+          {isOpen ? "-" : "+"}
+        </button>
       </div>
-      <div className="accordion-description">{description}</div>
+      {isOpen && <div className="accordion-description">{description}</div>}
     </div>
   );
 }
